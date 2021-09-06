e58003220e32538e3b8a50a38d8b2d5bdca1ebcb
sabn730 <66404408+sabn730@users.noreply.github.com>
2021-09-06 11:00:02 +0500

    Add files via upload

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,9 +1,27 @@
 import React, { useState } from 'react'
 
 export default function Counter() {
-    let [counter, setCounter] = useState(1)
+    // const [counter, setCounter] = useState(100)
+    const [userName, setName] = useState('Abdullah')
+    const [genName, setGenName] = useState(['Maaz', 'Abdullah', 'Asim', 'Hammad', 'Aqeel'])
 
+    // function addCounter() {
+    //     setCounter(counter + 1)
+    // }
+
+    // function minusCounter() {
+    //     setCounter(counter - 1)
+    // }
+
+    let randomName = () => {
+        let i = Math.floor(Math.random() * 5)
+        let newName = genName[i]
+        setName(newName)
+    }
     return (
-        <h1>{counter}</h1>
+        <div>
+            <h1>{userName}</h1>
+            <button onClick={randomName}>Generate Random Name</button>
+        </div>
     )
 }
